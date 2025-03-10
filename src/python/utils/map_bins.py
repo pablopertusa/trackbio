a8b402c0c72e7ebc523c1ded194f802610a89502
pablopertusa <pablopertusa27@gmail.com>
2025-03-10 12:24:59 +0100

    wip

---
+++ src/python/utils/map_bins.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+# Usar la función np.digitize para mapear al índice de una lista de bins, y luego recuperar el elemento.
+# Tener en cuenta el tipo de objeto de entrada, puede ser un xarray o una lista, hay que decidirlo.
