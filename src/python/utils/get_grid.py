b91875052530e3a8269beed9f4d62e2f0966fcfe
pablopertusa <pablopertusa27@gmail.com>
2025-03-21 17:53:14 +0100

    wip

--- src/python/utils/get_grid.py
+++ src/python/utils/get_grid.py
@@ -1,48 +1,23 @@
-import get_data
-import polars as pl
-from datetime import datetime
+import xarray as xr
+from pathlib import Path
+import numpy as np
 
-temp = pl.read_csv("data/temporal_subset.csv")
-box = pl.read_csv("data/world_box.csv")
+directory = "data/copernicus/raw"
+path = Path(directory)
+output = "data/copernicus/processed"
 
-day = temp.filter(pl.col("mas_actual") == 0)["day"].to_list()[0]
-month = temp.filter(pl.col("mas_actual") == 0)["month"].to_list()[0]
-year = temp.filter(pl.col("mas_actual") == 0)["year"].to_list()[0]
-min_time = f"{day}-{month}-{year}"
-day = temp.filter(pl.col("mas_actual") == 1)["day"].to_list()[0]
-month = temp.filter(pl.col("mas_actual") == 1)["month"].to_list()[0]
-year = temp.filter(pl.col("mas_actual") == 1)["year"].to_list()[0]
-max_time = f"{day}-{month}-{year}"
-fecha_min = datetime.strptime(min_time, "%d-%m-%Y")
-fecha_min_format = fecha_min.strftime("%Y-%m-%dT%H:%M:%S")
-fecha_max = datetime.strptime(max_time, "%d-%m-%Y")
-fecha_max_format = fecha_max.strftime("%Y-%m-%dT%H:%M:%S")
+for file in path.iterdir():
+    if file.name[-3:] == ".nc":
+        print("binning", file)
+        data = xr.open_dataset(file.absolute())
+        lat_bins = np.arange(data.latitude.min(), data.latitude.max() + 1, 1)
+        lon_bins = np.arange(data.longitude.min(), data.longitude.max() + 1, 1)
 
-max_latitude = box["max_latitude"].to_list()[0]
-min_latitude = box["min_latitude"].to_list()[0]
-max_longitude = box["max_longitude"].to_list()[0]
-min_longitude = box["min_longitude"].to_list()[0]
+        lat_labels = (lat_bins[:-1] + lat_bins[1:]) / 2
+        lon_labels = (lon_bins[:-1] + lon_bins[1:]) / 2
 
-print("empezamos")
-for y in range(fecha_min.year, fecha_max.year+1):
-    print("---------------------------")
-    print(f"AÑO: {y}")
-    if y == 1999:
-        fin = datetime.strptime("31-12-1999", "%d-%m-%Y")
-        fin_format = fin.strftime("%Y-%m-%dT%H:%M:%S")
-        get_data.make_request_copernicus("cmems_mod_glo_phy_my_0.083deg_P1D-m", f"Global_Ocean_Physics_Reanalysis_year_{y}.nc",
-                                                fecha_min_format, fin_format, max_latitude, min_latitude, max_longitude, min_longitude)
-    elif y == fecha_max.year:
-        inicio = datetime.strptime(f"1-1-{y}", "%d-%m-%Y")
-        inicio_format = fin.strftime("%Y-%m-%dT%H:%M:%S")
-        get_data.make_request_copernicus("cmems_mod_glo_phy_my_0.083deg_P1D-m", f"Global_Ocean_Physics_Reanalysis_year_{y}.nc",
-                                                inicio_format, fecha_max_format, max_latitude, min_latitude, max_longitude, min_longitude)
-    else:
-        inicio = datetime.strptime(f"1-1-{y}", "%d-%m-%Y")
-        fin = datetime.strptime(f"31-12-{y}", "%d-%m-%Y")
-        inicio_format = inicio.strftime("%Y-%m-%dT%H:%M:%S")
-        fin_format = fin.strftime("%Y-%m-%dT%H:%M:%S")
-        get_data.make_request_copernicus("cmems_mod_glo_phy_my_0.083deg_P1D-m", f"Global_Ocean_Physics_Reanalysis_year_{y}.nc",
-                                                inicio_format, fin_format, max_latitude, min_latitude, max_longitude, min_longitude)
-    print(f"AÑO: {y} FINALIZADO")
-print("Fin de descarga de datos")
+        data_binned = data.groupby_bins("latitude", lat_bins, labels=lat_labels).mean()
+        data_binned = data_binned.groupby_bins("longitude", lon_bins, labels=lon_labels).mean()
+
+        data_binned.to_netcdf(f"{output}/{file.name}_binned.nc")
+        print(file, "terminado")
---
+++ src/python/utils/make_requests.py
@@ -0,0 +1,53 @@
+import xarray as xr
+import copernicusmarine
+
+def make_request_copernicus(dataset, output_name, start_time, end_time, max_latitude, min_latitude, max_longitude, min_longitude, variables):
+    copernicusmarine.subset( 
+        dataset_id = dataset,
+        variables = variables,
+        minimum_longitude = min_longitude,
+        maximum_longitude = max_longitude,
+        minimum_latitude = min_latitude,
+        maximum_latitude = max_latitude,
+        start_datetime = start_time,
+        end_datetime = end_time,
+        minimum_depth = 0,
+        maximum_depth = 1,
+        output_filename = output_name,
+        output_directory = "/home/pablo/Desktop/zird/2/proy/github/data/copernicus/raw"
+    )
+
+def make_request_and_open_copernicus(dataset, output_name, start_time, end_time, max_latitude, min_latitude, max_longitude, min_longitude):
+    copernicusmarine.subset( 
+        dataset_id = dataset,
+        minimum_longitude = min_longitude,
+        maximum_longitude = max_longitude,
+        minimum_latitude = min_latitude,
+        maximum_latitude = max_latitude,
+        start_datetime = start_time,
+        end_datetime = end_time,
+        minimum_depth = 0,
+        maximum_depth = 1,
+        output_filename = output_name,
+        output_directory = "/home/pablo/Desktop/zird/2/proy/github/data/copernicus/raw"
+    )
+    nc_file = xr.open_dataset(f'/home/pablo/Desktop/zird/2/proy/github/data/copernicus/raw/{output_name}')
+    return nc_file
+
+def make_request_and_open_copernicus_with_variables(dataset, output_name, start_time, end_time, max_latitude, min_latitude, max_longitude, min_longitude, variables):
+    copernicusmarine.subset( 
+        dataset_id = dataset,
+        variables = variables,
+        minimum_longitude = min_longitude,
+        maximum_longitude = max_longitude,
+        minimum_latitude = min_latitude,
+        maximum_latitude = max_latitude,
+        start_datetime = start_time,
+        end_datetime = end_time,
+        minimum_depth = 0,
+        maximum_depth = 1,
+        output_filename = output_name,
+        output_directory = "/home/pablo/Desktop/zird/2/proy/github/data/copernicus/raw"
+    )
+    nc_file = xr.open_dataset(f'/home/pablo/Desktop/zird/2/proy/github/data/copernicus/raw/{output_name}')
+    return nc_file
