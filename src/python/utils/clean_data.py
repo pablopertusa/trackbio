bfaa7e30ebed20df17fe0775ea114669247d45db
pablopertusa <pablopertusa27@gmail.com>
2025-03-23 19:52:37 +0100

    wip

--- src/python/utils/clean_data.py
+++ src/python/utils/clean_data.py
@@ -1,11 +1,11 @@
 import xarray as xr
 
 data = xr.open_dataset("/home/pablo/Desktop/zird/2/proy/trackbio/data/copernicus/processed/dataset_concat.nc")
-data = data.drop_vars(["siconc", "sithick"])
-data = data.drop_duplicates(dim="time")
+data = data.drop_vars(["siconc", "sithick"]) # Estas variables eran todo NaN
+data = data.drop_duplicates(dim="time") # Había duplicados debido a cómo se hacían las requests
 print("duplicados eliminados")
 print("interpolando...")
-data = data.interpolate_na(dim="time", method="linear")
+data = data.interpolate_na(dim="time", method="linear") # Quitamos los NaN interpolando sobre la dimensión del tiempo
 
 data.to_netcdf("/home/pablo/Desktop/zird/2/proy/trackbio/data/copernicus/processed/clean_data.nc")
 print("dataset procesado")
---
+++ src/python/utils/get_data_by_location.py
@@ -0,0 +1,3 @@
+# Hacer función para obtener las variables medioambientales daba una localización
+
+# Hacer solamente si el modelo que vamos a usar requiere que los datos sean así y no en grid.
