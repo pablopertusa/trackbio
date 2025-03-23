fa262710f3ab3191f631c4ded967c5a15852a03e
pablopertusa <pablopertusa27@gmail.com>
2025-03-23 14:32:17 +0100

    wip

--- src/python/utils/make_requests.py
+++ src/python/utils/make_requests.py
@@ -1,10 +1,9 @@
 import xarray as xr
 import copernicusmarine
 
-def make_request_copernicus(dataset, output_name, start_time, end_time, max_latitude, min_latitude, max_longitude, min_longitude, variables):
+def make_request_copernicus(dataset, output_name, start_time, end_time, max_latitude, min_latitude, max_longitude, min_longitude):
     copernicusmarine.subset( 
         dataset_id = dataset,
-        variables = variables,
         minimum_longitude = min_longitude,
         maximum_longitude = max_longitude,
         minimum_latitude = min_latitude,
@@ -14,7 +13,7 @@ def make_request_copernicus(dataset, output_name, start_time, end_time, max_lati
         minimum_depth = 0,
         maximum_depth = 1,
         output_filename = output_name,
-        output_directory = "/home/pablo/Desktop/zird/2/proy/github/data/copernicus/raw"
+        output_directory = "/home/pablo/Desktop/zird/2/proy/trackbio/data/copernicus/raw"
     )
 
 def make_request_and_open_copernicus(dataset, output_name, start_time, end_time, max_latitude, min_latitude, max_longitude, min_longitude):
@@ -29,9 +28,9 @@ def make_request_and_open_copernicus(dataset, output_name, start_time, end_time,
         minimum_depth = 0,
         maximum_depth = 1,
         output_filename = output_name,
-        output_directory = "/home/pablo/Desktop/zird/2/proy/github/data/copernicus/raw"
+        output_directory = "/home/pablo/Desktop/zird/2/proy/trackbio/data/copernicus/raw"
     )
-    nc_file = xr.open_dataset(f'/home/pablo/Desktop/zird/2/proy/github/data/copernicus/raw/{output_name}')
+    nc_file = xr.open_dataset(f'/home/pablo/Desktop/zird/2/proy/trackbio/data/copernicus/raw/{output_name}')
     return nc_file
 
 def make_request_and_open_copernicus_with_variables(dataset, output_name, start_time, end_time, max_latitude, min_latitude, max_longitude, min_longitude, variables):
@@ -47,7 +46,7 @@ def make_request_and_open_copernicus_with_variables(dataset, output_name, start_
         minimum_depth = 0,
         maximum_depth = 1,
         output_filename = output_name,
-        output_directory = "/home/pablo/Desktop/zird/2/proy/github/data/copernicus/raw"
+        output_directory = "/home/pablo/Desktop/zird/2/proy/trackbio/data/copernicus/raw"
     )
-    nc_file = xr.open_dataset(f'/home/pablo/Desktop/zird/2/proy/github/data/copernicus/raw/{output_name}')
+    nc_file = xr.open_dataset(f'/home/pablo/Desktop/zird/2/proy/trackbio/data/copernicus/raw/{output_name}')
     return nc_file
