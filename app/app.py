0406213d56de6e373895403bba4d9ffeb4fc58c1
pablopertusa <pablopertusa27@gmail.com>
2025-04-09 19:05:56 +0200

    wip

--- app/app.py
+++ app/app.py
@@ -22,30 +22,42 @@ except KeyError:
 # Cogemos el subconjunto espacial y temporal
 success_subset = get_subset(animal_data, data_folder)
 
-box_path = data_folder + "world_box.csv"
-temp_path = data_folder + "temporal_subset.csv"
-output_directory = data_folder + "copernicus/raw"
-# Descargamos los datos
-success_download = download_data(copernicus_datasets, box_path, temp_path, output_directory)
+if success_subset:
+    box_path = data_folder + "world_box.csv"
+    temp_path = data_folder + "temporal_subset.csv"
+    output_directory = data_folder + "copernicus/raw"
+    # Descargamos los datos
+    success_download = download_data(copernicus_datasets, box_path, temp_path, output_directory)
+else:
+    print("Error en download, no continuamos")
 
-input_directory = data_folder + "copernicus/raw"
-output_directory = data_folder + "copernicus/processed"
-df_world_box = pl.read_csv(box_path)
-lat_max = df_world_box["max_latitude"].to_list()[0]
-lat_min = df_world_box["min_latitude"].to_list()[0]
-lon_max = df_world_box["max_longitude"].to_list()[0]
-lon_min = df_world_box["min_longitude"].to_list()[0]
-# Hacemos el grid de los archivos más grande para que todos coincidan y sean manejables
-success_binning = make_grid_files(input_directory, output_directory, grid_size, lat_min, lat_max, lon_min, lon_max)
+if success_download:
+    input_directory = data_folder + "copernicus/raw"
+    output_directory = data_folder + "copernicus/processed"
+    df_world_box = pl.read_csv(box_path)
+    lat_max = df_world_box["max_latitude"].to_list()[0]
+    lat_min = df_world_box["min_latitude"].to_list()[0]
+    lon_max = df_world_box["max_longitude"].to_list()[0]
+    lon_min = df_world_box["min_longitude"].to_list()[0]
+    # Hacemos el grid de los archivos más grande para que todos coincidan y sean manejables
+    success_binning = make_grid_files(input_directory, output_directory, grid_size, lat_min, lat_max, lon_min, lon_max)
+else:
+    print("Error en binning, no continuamos")
 
-input_directory = data_folder + "copernicus/processed"
-df_temp = pl.read_csv(temp_path)
-min_year = df_temp.filter(pl.col("first") == 1)["year"].to_list()[0]
-max_year = df_temp.filter(pl.col("first") == 0)["year"].to_list()[0]
-success_concat_list = []
-for dataset in copernicus_datasets:
-    # Concatenamos todos los años de cada dataset
-    success_aux = concat_year(input_directory, dataset, min_year, max_year)
-    success_concat_list.append(success_aux)
-success_concat = all(success_concat_list)
+if success_binning:
+    input_directory = data_folder + "copernicus/processed"
+    df_temp = pl.read_csv(temp_path)
+    min_year = df_temp.filter(pl.col("first") == 1)["year"].to_list()[0]
+    max_year = df_temp.filter(pl.col("first") == 0)["year"].to_list()[0]
+    success_concat_list = []
+    for dataset in copernicus_datasets:
+        # Concatenamos todos los años de cada dataset
+        success_aux = concat_year(input_directory, dataset, min_year, max_year)
+        success_concat_list.append(success_aux)
+    success_concat = all(success_concat_list)
+else:
+    print("Error en concat, no continuamos")
+
+if success_concat:
+    print("Hemos llegado al final")
 
