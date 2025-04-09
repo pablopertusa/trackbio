bf96712e1741d13311a9ed94c1c1d398ba620320
pablopertusa <pablopertusa27@gmail.com>
2025-04-09 13:49:33 +0200

    wip

--- src/python/utils/tracking_to_netCDF.py
+++ src/python/utils/tracking_to_netCDF.py
@@ -2,6 +2,9 @@ import polars as pl
 import numpy as np
 import xarray as xr
 import pandas as pd  # solo para fechas (xarray no acepta directamente Series de Polars)
+from datetime import datetime
+
+testing = True
 
 schema = {
     "individual_id": pl.String,
@@ -15,42 +18,63 @@ schema = {
 }
 
 df = pl.read_csv("data/foca_procesado.csv", schema=schema)
+# Aplicamos el mismo filtrado que se hizo para sacar el subconjunto de datos espacial y temporal en data_subset.ipynb
+df = df.filter(pl.col("year") >= 2005) # Quitar algunos años para que el dataset se pueda tratar en local
+df = df.filter(pl.col("decimal_longitude") >= -90).filter(pl.col("decimal_longitude") <= 90) # Quitar outliers
 copernicus_data = xr.open_dataset("data/copernicus/processed/Global_Ocean_Physics_Reanalysis_clean.nc")
+
+def encontrar_bin(valor, bins):
+    idx = np.digitize(valor, bins) - 1  # Restamos 1 para que el índice sea coherente con el bin izquierdo
+    if idx < 0:
+        idx = 0
+    if idx >= len(bins) - 1:
+        return None  # El valor está fuera del rango de bins
+    return bins[idx]
 
 # Redondear coordenadas a la grid de 1°
 df = df.with_columns([
-    (pl.col("decimal_latitude").floor()).cast(pl.Int32).alias("lat_bin"),
-    (pl.col("decimal_longitude").floor()).cast(pl.Int32).alias("lon_bin"),
-    (pl.col("date").dt.truncate("1d")).alias("datetime_day")  # Redondeo a hora
+    (pl.col("decimal_latitude").map_elements(lambda x: encontrar_bin(x, copernicus_data["latitude_bins"].values), return_dtype=pl.Float64)).alias("lat_bin"),
+    (pl.col("decimal_longitude").map_elements(lambda x: encontrar_bin(x, copernicus_data["longitude_bins"].values), return_dtype=pl.Float64)).alias("lon_bin"),
+    (pl.col("date").dt.truncate("1d")).alias("datetime_day")  # Redondeo a día
 ])
+print(df)
 
 # Extraer coordenadas únicas
-lat_vals = np.arange(-70, -29)  # -70 a -30 inclusive
-lon_vals = np.arange(-70, -29)
+lat_vals = copernicus_data["latitude_bins"].values
+lon_vals = copernicus_data["longitude_bins"].values
+print(lat_vals)
+print(lon_vals)
 
 # Obtener tiempos únicos
-unique_times = df.select("datetime_day").unique().sort("datetime_day").to_series().to_list()
+unique_times = copernicus_data["time"].values
 time_to_idx = {t: i for i, t in enumerate(unique_times)}
 
-# 5. Crear índice de lat y lon
+# Crear índice de lat y lon
 lat_to_idx = {lat: i for i, lat in enumerate(lat_vals)}
 lon_to_idx = {lon: i for i, lon in enumerate(lon_vals)}
 
-# 6. Crear array de ceros
+# Crear array de ceros (esto es el grid que se irá rellenando con las observaciones)
 data = np.zeros((len(unique_times), len(lat_vals), len(lon_vals)), dtype=np.uint8)
 
-# 7. Iterar y marcar presencia
+# Iterar y marcar presencia
+not_found = 0
 for row in df.iter_rows(named=True):
-    t = row["datetime_hour"]
+    t = row["datetime_day"]
+    t_parsed = np.datetime64(t)
     lat = row["lat_bin"]
     lon = row["lon_bin"]
-    if lat in lat_to_idx and lon in lon_to_idx and t in time_to_idx:
-        i = time_to_idx[t]
+
+    if lat in lat_to_idx and lon in lon_to_idx and t_parsed in time_to_idx:
+        i = time_to_idx[t_parsed]
         j = lat_to_idx[lat]
         k = lon_to_idx[lon]
         data[i, j, k] = 1
+    else:
+        not_found += 1
 
-# 8. Crear dataset xarray
+print("Observaciones no encontradas:", not_found)
+
+# Crear dataset xarray
 ds = xr.Dataset(
     {
         "presencia": (["time", "latitude", "longitude"], data)
@@ -62,5 +86,7 @@ ds = xr.Dataset(
     }
 )
 
-# 9. Guardar como NetCDF
-ds.to_netcdf("salida_polars.nc")
+print(ds)
+
+# Guardar como NetCDF
+ds.to_netcdf("data/grid_presencia.nc")
